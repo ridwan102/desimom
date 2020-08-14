da0338159d52944d754c6f0b3a088863d5066b84
Ridwan Alam <ridwan102@gmail.com>
2020-08-14 18:17:03 -0400

    Fixed logo issue on Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Intro from './Intro'
 import Menu from './Menu'
-import logo from "../images/logo.jpeg";
+// import logo from "../images/logo.jpeg";
 
 export default class Home extends Component {
     render() {
